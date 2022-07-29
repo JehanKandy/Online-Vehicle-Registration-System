33618f1c81c14551a961a1de382c3a8387435e93
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-07-29 19:16:55 +0530

    Update login.php

--- lib/views/login.php
+++ lib/views/login.php
@@ -1,4 +1,12 @@
 <link rel="stylesheet" href="../../css/style.css">
-
 <?php include("../layouts/header.php"); ?>
-<?php include("../layouts/nav_login.php"); ?>
+<?php include("../layouts/nav_reg.php"); ?>
+
+
+<div class="login">
+    <div class="login-content">
+        <div class="title">
+            <i class="fas fa-user-alt"></i>login
+        </div>
+    </div>
+</div>
