c2a36cfd6347eb9de1464399f9de38694f9ced91
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-07-29 14:24:09 +0530

    Update index.php

--- index.php
+++ index.php
@@ -61,23 +61,11 @@
         Partnership
     </div>
     <div class="grid">
-        <div class="partner-item1">
-            HI
-        </div>
-        <div class="partner-item2">
-            HI
-        </div>
-        <div class="partner-item3">
-            HI
-        </div>
-        <div class="partner-item4">
-            HI
-        </div>
-        <div class="partner-item5">
-            HI
-        </div>
-        <div class="partner-item6">
-            HI
-        </div>
+        <div class="partner-item1"></div>
+        <div class="partner-item2"></div>
+        <div class="partner-item3"></div>
+        <div class="partner-item4"></div>
+        <div class="partner-item5"></div>
+        <div class="partner-item6"></div>
     </div>
 </div>
