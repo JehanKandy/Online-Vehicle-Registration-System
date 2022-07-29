53f2579ef8bc69a35cd9ab3c4c350b55f497b45d
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-07-29 19:27:03 +0530

    Update login.php

--- lib/views/login.php
+++ lib/views/login.php
@@ -4,25 +4,27 @@
 <?php include("../layouts/nav_login.php"); ?>
 
 <div class="login">
-    <div class="login-content">
-        <div class="title">
-            <i class="fas fa-user-alt"></i>&nbsp;Login
-            <hr>
-        </div>
-        <div class="body">
-            <form action="<?php echo($_SERVER['PHP_SELF']); ?>" method="POST">
-                <div class="login-data">
-                    <label for="username">Username : </label><br>
-                    <input type="text" name="username" id="username" class="login-input">
-                </div>
-                <div class="login-data">
-                    <label for="password">Password : </label><br>
-                    <input type="password" name="password" id="password" class="login-input">        
-                </div>
-                <div class="login-data">
-                    <input type="submit" value="Login" name="login" class="login-submit">
-                </div>                    
-            </form>
+    <div class="login-box">
+        <div class="login-content">
+            <div class="title">
+                <i class="fas fa-user-alt"></i>&nbsp;Login
+                <hr>
+            </div>
+            <div class="body">
+                <form action="<?php echo($_SERVER['PHP_SELF']); ?>" method="POST">
+                    <div class="login-data">
+                        <label for="username">Username : </label><br>
+                        <input type="text" name="username" id="username" class="login-input">
+                    </div>
+                    <div class="login-data">
+                        <label for="password">Password : </label><br>
+                        <input type="password" name="password" id="password" class="login-input">        
+                    </div>
+                    <div class="login-data">
+                        <input type="submit" value="Login" name="login" class="login-submit">
+                    </div>                    
+                </form>
+            </div>
         </div>
     </div>
 </div>
