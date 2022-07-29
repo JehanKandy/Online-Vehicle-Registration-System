42e43b5af176b765b2e084b82fd8f4978602547a
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-07-29 13:05:58 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php include("lib/layouts/header.php"); ?>
+<?php include("lib/layouts/nav.php"); ?>
+
+<div class="home">
+    <div class="home-content">
+        Welcome To 
+        <div class="title">Vehicle Registration System</div>
+    </div>
+</div>
