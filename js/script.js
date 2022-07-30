419391a84cbd5f1775831905e8469684839732f7
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-07-30 09:42:14 +0530

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const menu_toggle = document.querySelector('.menu-toggle');
+const sidebar = document.querySelector('.sidebar');
+
+menu_toggle.addEventListener('click', () => {
+    menu_toggle.classList.toggle('is-active');
+    sidebar.classList.toggle('is-active');
+});
