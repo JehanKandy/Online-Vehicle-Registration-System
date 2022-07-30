d5273644c23907ac705e53dad3368574895f1ef4
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-07-30 13:06:29 +0530

    Update admin.php

--- lib/routes/admin.php
+++ lib/routes/admin.php
@@ -17,6 +17,7 @@
 			<a href="#" class="menu-item"><i class="fas fa-tachometer-alt"></i>Dashboard</a>
 			<a href="#" class="menu-item"><i class="fas fa-user-alt"></i>Users</a>
 			<a href="#" class="menu-item"><i class="fas fa-user-tie"></i>Admins</a>
+			<a href="#" class="menu-item"><i class="fas fa-user-tag"></i>Staff</a>
 			<a href="#" class="menu-item"><i class="fas fa-car-alt"></i>Vehicles</a>
 			<a href="#" class="menu-item"><i class="fas fa-user-cog"></i>Account Settings</a>
 		</nav>
@@ -25,6 +26,19 @@
 
 	<main class="content">
 		<h1>Welcome, To Admin Dashboard</h1>
+		<hr>
+		<div class="admin-content">
+			<div class="grid">
+				<div class="admin-item1"></div>
+				<div class="admin-item2"></div>
+				<div class="admin-item3"></div>
+				<div class="admin-item4"></div>
+				<div class="admin-item5"></div>
+				<div class="admin-item6"></div>
+				<div class="admin-item7"></div>
+				<div class="admin-item8"></div>
+			</div>
+		</div>
 	</main>
 </div>
 
