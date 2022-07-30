7c66fac7dc89ae1bcc9a4cf13faedf7bcf648c53
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-07-30 09:34:59 +0530

    Add files via upload

---
+++ lib/routes/admin.php
@@ -0,0 +1,31 @@
+<link rel="stylesheet" href="../../css/dashboard.css">
+<link rel="stylesheet" href="../../css/style.css">
+<?php include("../layouts/header.php"); ?>
+<?php include("../layouts/nav_loged.php"); ?>
+
+<div class="app">
+	<div class="menu-toggle">
+		<div class="hamburger">
+			<span></span>
+		</div>
+	</div>
+
+	<aside class="sidebar">
+		<nav class="menu">
+			<img src="../../1.PNG" alt="Profile" class="profile-img">
+			<p class="profile-name">JehanKandy</p>
+			<a href="#" class="menu-item"><i class="fas fa-tachometer-alt"></i>Dashboard</a>
+			<a href="#" class="menu-item"><i class="fas fa-user-alt"></i>Users</a>
+			<a href="#" class="menu-item"><i class="fas fa-user-tie"></i>Admins</a>
+			<a href="#" class="menu-item"><i class="fas fa-car-alt"></i>Vehicles</a>
+			<a href="#" class="menu-item"><i class="fas fa-user-cog"></i>Account Settings</a>
+		</nav>
+
+	</aside>
+
+	<main class="content">
+		<h1>Welcome, To Admin Dashboard</h1>
+	</main>
+</div>
+
+<script src="../../js/script.js"></script>
