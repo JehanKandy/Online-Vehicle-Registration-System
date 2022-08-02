6de2f951e7e2a2e3802b76b70284d99ef7dfe584
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-08-02 19:38:34 +0530

    Update config.php

--- lib/function/config.php
+++ lib/function/config.php
@@ -1,11 +1,11 @@
 <?php 
-    function connection(){
+    function Connection(){
         $server = "localhost";
         $user = "root";
         $pass = "";
-        $db_name = "e_bill_system";
+        $db_name = "vehicle_reg_system";
 
-        $con = mysqli_connect($server,$user,$pass,$db_name);
+        $con = mysqli_connect($server, $user, $pass, $db_name);
 
         $result = (!$con)?"Connection Lost":$con;
         return $result;
