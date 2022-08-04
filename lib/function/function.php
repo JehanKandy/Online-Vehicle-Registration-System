0add62e7a1ec082962fe1519ff50aa228058d49e
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-08-04 18:33:49 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -10,11 +10,21 @@
         $check_user_result = mysqli_query($con,$check_user);
         $user_check_count = mysqli_num_rows($check_user_result);
 
-        if()
-
-        
-
+        if($user_check_count > 0){
+            return "<center>&nbsp<div class='alert alert-danger col-10' role='alert'>User Already Exists..!</div>&nbsp</center>";
+        }
+        else{
+            if($pass1 != $cpass){
+                return "<center>&nbsp<div class='alert alert-danger col-10' role='alert'>Passwords are Doesn't Match....!/div>&nbsp</center>";
+            }
+            else{
+                $insert_data = "INSERT INTO user_tbl(username,email,pass_user,gender)VALUES('$username','$email','$pass1','$gender')";
+                $insert_data_result = mysqli_query($con, $insert_data);
+                header("location:../views/login.php");
+            }
+        } 
     }
+
     
 
 
