e2ee6849dc7015ab82b9a4be7d1750523cf37f2b
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-08-04 18:23:30 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -3,9 +3,17 @@
     use FTP\Connection;
     session_start();
 
-    function user_reg(){
+    function user_reg($username,$email,$gender,$pass1,$cpass){
         $con = Connection();
         
+        $check_user = "SELECT * FROM user_tbl WHERE username = '$username' && email = '$email'";
+        $check_user_result = mysqli_query($con,$check_user);
+        $user_check_count = mysqli_num_rows($check_user_result);
+
+        if()
+
+        
+
     }
     
 
