58218933d93a9562a741b8464f5cdeea29675918
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-08-04 19:03:17 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -30,12 +30,22 @@
         $check_user_login = "SELECT * FROM user_tbl WHERE username='$login_username' && pass_user='$login_pass'";
         $check_user_login_result = mysqli_query($con, $check_user_login);
         $user_login_count = mysqli_num_rows($check_user_login_result);
-        $user_login_row = mysqli_fetch_assoc($check_user_login_result);
+        $user_login_row = mysqli_fetch_assoc($check_user_login_result); 
 
-        
-
-
-
+        if($user_login_count > 0){
+            if($login_pass != $user_login_row['pass_user']){
+                return "<center>&nbsp<div class='alert alert-danger col-10' role='alert'>Password is Does't Match...!</div>&nbsp</center>";
+            }
+            else{
+                if($user_login_row['roll'] == 'user'){
+                    setcookie('login',$user_login_row['email'],time()+60*60,'/');
+                    $_SESSION['LoginSession'] = $user_login_row['email'];
+                    header("../routes/admin.php");                  
+                }
+            }
+        }else{
+            return "<center>&nbsp<div class='alert alert-danger col-10' role='alert'>User Doesn't Exists..!</div>&nbsp</center>";
+        }    
     }
 
     
