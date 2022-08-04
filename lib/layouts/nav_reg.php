0e20d25cdf56589702659e48c1ee1191aafc4917
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-08-04 19:08:01 +0530

    Add files via upload

---
+++ lib/layouts/nav_reg.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+    <a class="navbar-brand" href="#">&nbsp;&nbsp;Vehicle Registration System</a>
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+    </ul>
+    <span class="navbar-text">
+        <a href="login.php"><button class="login-btn-nav"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+  <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+</svg> &nbsp;Login</button></a>&nbsp;&nbsp;&nbsp;&nbsp;
+    </span>
+  </div>
+</nav>
