bf0cde8af0b401de0bb27d5523ac5efc3222b617
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-08-11 09:52:58 +0530

    Add files via upload

---
+++ lib/layouts/nav_loged_user.php
@@ -0,0 +1,14 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+    <a class="navbar-brand" href="#">&nbsp;&nbsp;Vehicle Registration System</a>
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+    </ul>
+    <span class="navbar-text">
+        <span><?php ?> JehanKandy &nbsp;&nbsp;&nbsp;&nbsp;</span>   
+        <a href="../../views/logout.php"><button class="logout-btn-nav">  &nbsp;Logout</button></a>&nbsp;&nbsp;&nbsp;&nbsp;
+    </span>
+  </div>
+</nav>
