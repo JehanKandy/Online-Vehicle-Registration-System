3e2d9085245aad37d6705bf9564e98d1853363f0
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-08-19 20:31:50 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php include("lib/layouts/header_index.php"); ?>
+<?php include("lib/layouts/nav_index.php"); ?>
+
+
+<div class="partners">
+    <div class="partners-content">
+        Partnership
+    </div>
+    <div class="grid">
+        <div class="partner-item1"></div>
+        <div class="partner-item2"></div>
+        <div class="partner-item3"></div>
+        <div class="partner-item4"></div>
+        <div class="partner-item5"></div>
+        <div class="partner-item6"></div>
+    </div>
+</div>
+
+
+<?php include("lib/layouts/footer.php"); ?>
