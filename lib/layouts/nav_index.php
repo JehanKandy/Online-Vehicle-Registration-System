aa1e3dbf712c643b4a46ce49b270ce3f1983f087
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-08-19 20:56:36 +0530

    Add files via upload

---
+++ lib/layouts/nav_index.php
@@ -0,0 +1,13 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+    <a class="navbar-brand" href="#">&nbsp;&nbsp;Project Alpha</a>
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+    </ul>
+    <span class="navbar-text">
+        Ver 1.0 
+    </span>
+  </div>
+</nav>
