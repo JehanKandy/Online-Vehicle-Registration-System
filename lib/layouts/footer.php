766a2af8bb6fb9d7814870fbc205ffaf20cea8de
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-08-19 20:18:43 +0530

    Update footer.php

--- lib/layouts/footer.php
+++ lib/layouts/footer.php
@@ -4,21 +4,16 @@
         <div class="container text-center text-md-left">
             <div class="row text-center text-md-left">
                 <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt3">
-                    <h5 class="text-uppercase md-4 font-wight-bold text-warning">Vehicle Registration</h5>
-
-                </div>
-                <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt3">
                     <h5 class="text-uppercase md-4 font-wight-bold text-warning">About Us</h5>
                     <p>Register Your Vehicle with the System for Register fuel and other future Registrations. This membership vaild for life-time</p>
                 </div>
                 <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt3">
                     <h5 class="text-uppercase md-4 font-wight-bold text-warning">Partnership</h5>
-                    <p><a href="#" class="text-white" style="text-decoration: none;">Salinda organizations</a></p>
-                    <p><a href="#" class="text-white" style="text-decoration: none;">Santhush organizations</a></p>
-                    <p><a href="#" class="text-white" style="text-decoration: none;">Salinda organizations</a></p>
-                    <p><a href="#" class="text-white" style="text-decoration: none;">Santhush organizations</a></p>
-                    <p><a href="#" class="text-white" style="text-decoration: none;">Salinda organizations</a></p>
-                    <p><a href="#" class="text-white" style="text-decoration: none;">Santhush organizations</a></p>
+                    <p><a href="#" class="text-white" style="text-decoration: none;">ABC organizations</a></p>
+                    <p><a href="#" class="text-white" style="text-decoration: none;">ABC organizations</a></p>
+                    <p><a href="#" class="text-white" style="text-decoration: none;">ABC organizations</a></p>
+                    <p><a href="#" class="text-white" style="text-decoration: none;">ABC organizations</a></p>
+                    <p><a href="#" class="text-white" style="text-decoration: none;">ABC organizations</a></p>
                 </div>
                 <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt3">
                     <h5 class="text-uppercase md-4 font-wight-bold text-warning">contact details</h5>
@@ -28,10 +23,10 @@
                 </div>
                 <hr class="mb-4">
                 <div class="col-md-7 col-lg-8">
-                    <p>Copyright <i class="far fa-copyright"></i> 2022 All Right Reserved by : 
+                    <p>Copyright <i class="far fa-copyright"></i> Project Alpha : 2022 All Right Reserved by : 
                         <a href="https://github.com/JehanKandy" target="_blank" style="text-decoration:none;" class="text-warning">Jehankandy</a></p>
                 </div>
-                <div class="col-md-5 col-lg-4">
+                <div class="col-md-4 col-lg-3">
                     <div class="text-center text-md-right">
                         <ul>
                             <li class="list-inline-item">
@@ -39,12 +34,6 @@
                             </li>
                             <li class="list-inline-item">
                                 <a href="https://www.linkedin.com/in/jehan-kandy-23a3a4223/" target="_blank" class="btn-floating btn-sm text-white" style="font-size:23px;"><i class="fab fa-linkedin"></i></a>    
-                            </li>
-                            <li class="list-inline-item">
-                                <a href="https://www.youtube.com/channel/UC675lo49LTHGi9SCX8XbM5g" target="_blank" class="btn-floating btn-sm text-white" style="font-size:23px;"><i class="fab fa-youtube"></i></a>    
-                            </li>
-                            <li class="list-inline-item">
-                                <a href="https://www.facebook.com/jayaba.jehan" target="_blank" class="btn-floating btn-sm text-white" style="font-size:23px;"><i class="fab fa-facebook-square"></i></a>    
                             </li>
                             <li class="list-inline-item">
                                 <a href="https://wa.me/94711758851?text=%20More%20information%20about%20vehicle%20Registation%20system" target="_blank" class="btn-floating btn-sm text-white" style="font-size:23px;"><i class="fab fa-whatsapp-square"></i></a>    
