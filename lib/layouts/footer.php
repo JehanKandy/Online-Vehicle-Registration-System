f00a53d1ac1d68195a20295192176996acc73b9b
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-08-19 20:20:13 +0530

    Update footer.php

--- lib/layouts/footer.php
+++ lib/layouts/footer.php
@@ -4,7 +4,7 @@
         <div class="container text-center text-md-left">
             <div class="row text-center text-md-left">
                 <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt3">
-                    <h5 class="text-uppercase md-4 font-wight-bold text-warning">About Us</h5>
+                    <h5 class="text-uppercase md-4 font-wight-bold text-warning">Project Alpha</h5>
                     <p>Register Your Vehicle with the System for Register fuel and other future Registrations. This membership vaild for life-time</p>
                 </div>
                 <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt3">
