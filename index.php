8211c88e74d8765e9cc97bf5f96bff9a1a56f13a
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-08-21 08:36:36 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,41 @@
 <?php include "lib/layouts/header_index.php";?>
 <?php include "lib/layouts/nav_index.php";?>
 
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="images/car1.jpg" alt="First slide">
+        <div class="carousel-caption d-none d-md-block">
+            <p>JK BOOTSTRAP</p>
+        </div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="images/car2.jpg" alt="Second slide">
+      <div class="carousel-caption d-none d-md-block">
+
+        </div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="images/car3.jpg" alt="Third slide">
+      <div class="carousel-caption d-none d-md-block">
+
+        </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
 
 <div class="contact">
     <div class="contact-content">
